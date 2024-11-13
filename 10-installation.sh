987fee2647d08f6918944cc4ebb8b39efc138553
Putti Swamy <puttiswamy123@gmail.com>
2024-11-13 15:50:46 +0530

    sample file

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-dnf install mysql -y
+USERID=$(id -u)
+
+if [ $USERID -eq 0 ]
+then
+   echo "you are a super user "
+   dnf install mysql -y
+else 
+   echo " You are not a super user,please run the script with root access"
+   exit 1
+fi
