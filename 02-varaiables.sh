532e37aa25cd14967cfd69b0732519d31c4446ad
Putti Swamy <puttiswamy123@gmail.com>
2024-11-13 12:48:17 +0530

    variable file

---
+++ 02-varaiables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo " Ramesh:: Hello Suresh,How are you"
+echo " Suresh:: Hi Ramesh, I am fine. How are you"
+echo " Ramesh :: I am fine too, how is your work?"
+echo " Suresh:: not bad. I am upgrading to DevOps"
+
+
+ 
