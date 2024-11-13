414285f2d43fa5a3447e0f796608fa0c7b0ee549
Putti Swamy <puttiswamy123@gmail.com>
2024-11-13 14:29:06 +0530

    sample file

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-MOVIES=( "RRR", "DJTillu", "Murari" )
+MOVIES=( "RRR" "DJTillu" "Murari" )
 
 # size of above array is 3
 # index are 0,1,2
 
 echo " First movie is: ${MOVIES[0]} "
+
+echo " All movies are : ${@} "
