5ca30d157b942270050308ba55ec5f5072db5808
Putti Swamy <puttiswamy123@gmail.com>
2024-11-13 15:59:18 +0530

    sample file

--- 10-installation.sh
+++ 10-installation.sh
@@ -11,7 +11,17 @@ then
       echo "Installation of mysql is success"
     else
       echo "Installation of mysql is failure"
+      exit 1
     fi
+    dnf install git -y
+    if [ $? -eq 0 ]
+    then
+      echo "Installation of git is success"
+    else
+      echo "Installation of git is failure"
+      exit 1
+    fi
+    
 else 
    echo " You are not a super user,please run the script with root access"
    exit 1
