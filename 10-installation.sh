656076d41d07c32022f49a0a5911a44ff4c78c55
Putti Swamy <puttiswamy123@gmail.com>
2024-11-13 15:54:47 +0530

    sample file

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,6 +6,12 @@ if [ $USERID -eq 0 ]
 then
    echo "you are a super user "
    dnf install mysql -y
+   if [ $? -eq 0 ]
+   then
+      echo "Installation of mysql is success"
+    else
+      echo "Installation of mysql is failure"
+    fi
 else 
    echo " You are not a super user,please run the script with root access"
    exit 1
