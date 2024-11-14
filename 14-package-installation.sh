fb4db63b783151d2a82003614d974aca319beb1f
Putti Swamy <puttiswamy123@gmail.com>
2024-11-14 12:18:17 +0530

    sample file

--- 14-package-installation.sh
+++ 14-package-installation.sh
@@ -6,6 +6,23 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE() {
+   if [ $1 -eq 0 ]
+   then 
+      echo " $2 SUCCESS "
+   else
+      echo  " $2 FAILURE "
+      exit
+    fi
+
+}
 
 if [ $USERID -eq 0 ]
 then
@@ -18,4 +35,11 @@ fi
 for i in $@
 do
    echo "package to install: $i"
+   dnf list installed $i &>>$LOGFILE
+   if [ $? -eq 0 ]
+   then 
+      echo " $i already installed... skipping "
+   else
+      echo " $i not installed... Need to install"
+    fi
 done
