84719aba6e32849c47a3afe7d443fd964eb7aeab
Putti Swamy <puttiswamy123@gmail.com>
2024-11-19 10:58:52 +0530

    scripts

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -7,6 +7,7 @@ echo " Process ID of current shell script: $$ "
 
 #./16-other-script.sh
 
+source./16-other-script.sh
 
 echo "After calling other script, Course: $COURSE "
 echo " Process ID of current shell script: $$ "
